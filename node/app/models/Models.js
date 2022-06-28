f8ed238e2566040fc94b6ade3382d8dbc5ec1f8a
hezirel <80560775+hezirel@users.noreply.github.com>
2022-06-28 03:27:39 +0200

    Slight change to _id lookup for client

--- node/app/models/Models.js
+++ node/app/models/Models.js
@@ -9,47 +9,44 @@ const locationSchema = new mongoose.Schema({
   label: {type: String, required: false},
   clientid: {type: String, required: false}
 });
-
+const Location = mongoose.model('locations', locationSchema);
 
 const driverSchema = new mongoose.Schema({
   name: {type: String, required: true},
   age: {type: Number, required: true}
 });
+const Driver = mongoose.model('drivers', driverSchema);
 
 const vehicleSchema = new mongoose.Schema({
   brand: {type: String, required: true},
   model: {type: String, required: true},
   plate: {type: String, required: true}
 });
+const Vehicle = mongoose.model('vehicles', vehicleSchema);
 
 const clientSchema = new mongoose.Schema({
   clientid: {type: String, required: true},
   locations: [locationSchema]
 });
+const Client = mongoose.model('clients', clientSchema);
 
 const placeTimeSchema = new mongoose.Schema({
   place: {type: locationSchema, required: true},
   date: {type: Date, required: true, default: Date.now}
 });
-
-const Driver = mongoose.model('drivers', driverSchema);
-const Client = mongoose.model('clients', clientSchema);
+const PlaceTime = mongoose.model('placetimes', placeTimeSchema);
 
 const deliverySchema = new mongoose.Schema({
   client: {type: mongoose.Schema.Types.ObjectId, ref: 'clients', required: false},
   driver: {type: mongoose.Schema.Types.ObjectId, ref: 'drivers', required: true},
-  vehicle: {type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle', required: false},
+  vehicle: {type: mongoose.Schema.Types.ObjectId, ref: 'vehicles', required: false},
   pickup: {type: placeTimeSchema, required: false},
   dropoff: {type: placeTimeSchema, required: false},
   pickupDate: {type: Date, required: false},
   deliveryDate: {type: Date, required: false},
   distance: {type: Number, required: false}
 });
-
-const Location = mongoose.model('locations', locationSchema);
-const Vehicle = mongoose.model('vehicles', vehicleSchema);
 const Delivery = mongoose.model('deliveries', deliverySchema);
-const PlaceTime = mongoose.model('placetimes', placeTimeSchema);
 
 module.exports = {
   Driver,
